116e1103167efa95f088b31998547ddb1f8aa1eb
Abubakar Abid <a12d@stanford.edu>
2020-10-29 17:08:30 -0500

    regex to identify port

--- clean-connections.py
+++ clean-connections.py
@@ -4,6 +4,6 @@ import re
 pipe = Popen('sudo netstat -tnp | grep -i "paramik"', shell=True, stdout=PIPE)
 for line in pipe.stdout:
     line = line.strip()
-    pid = re.findall(r"\d+\/sshd")[0][:-5]
+    pid = re.findall(r"\d+\/sshd", line)[0][:-5]
     print(line)
     print(pid)
