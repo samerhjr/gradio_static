73aa320ef4f91d7324cca004f4bfc568f828e4f9
Abubakar Abid <a12d@stanford.edu>
2020-10-29 17:13:10 -0500

    regex to identify port

--- clean-connections.py
+++ clean-connections.py
@@ -5,6 +5,6 @@ pipe = Popen('sudo netstat -tnp | grep -i "paramik"', shell=True, stdout=PIPE)
 for line in pipe.stdout:
     line = line.strip()
     pid = re.findall(r"\d+\/sshd", line)[0][:-5]
-    duration = Popen('ps -o etime= -p {}'.format(pid), shell=True, stdout=PIPE).stdout
+    duration = Popen('ps -o etime= -p {}'.format(pid), shell=True, stdout=PIPE)
     print(duration.stdout)
     print(pid, duration)
